3799839c9ca407e9ca115bc1bf286728d935c0b6
LestherHD <wmentel@miumg.edu.gt>
2024-06-24 00:01:58 -0600

    Corrección de formatos de precios

--- src/app/utils/FunctionsUtils.ts
+++ src/app/utils/FunctionsUtils.ts
@@ -247,6 +247,19 @@ export class FunctionsUtils {
     return 0;
   }
 
+  formatPrice(price: number): string {
+    // Si el número no tiene decimales, agregar ".00"
+    if (price % 1 === 0) {
+      return price.toFixed(2);
+    }
+    // Si tiene un solo decimal, agregar un cero adicional al final
+    if (price % 1 === 0.1) {
+      return price.toFixed(1) + '0';
+    }
+    // Si tiene dos decimales, retornar el número tal cual
+    return price.toFixed(2);
+  }
+
 
 
 }
