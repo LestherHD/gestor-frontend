6e5d26387f555c114618ca470ba1a74fea7160cc
LestherHD <wmentel@miumg.edu.gt>
2024-07-17 20:48:36 -0600

    Pruebas de imagenes

--- docs/browser/chunk-C6TJHT6G.js
+++
@@ -1,1 +0,0 @@
-var i=class{constructor(s,t,o,c,h,r,a,e){this.usuario=s,this.correo=t,this.codigo=o,this.contrasenia=c,this.tipoRecuperacion=h,this.usuarios=r,this.page=a,this.size=e}};export{i as a};
