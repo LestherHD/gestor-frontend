f18c045d3a2ff8c95aeddcd51724ba7d91e9cc7d
LestherHD <wmentel@miumg.edu.gt>
2024-08-09 13:56:04 -0600

    Corrección en mensajes de validación al registrar usuario

--- src/app/views/pages/register/register.component.ts
+++ src/app/views/pages/register/register.component.ts
@@ -32,6 +32,7 @@ import {Router} from '@angular/router';
 import {UsuariosRequestDTO} from '../../../dto/UsuariosRequestDTO';
 import {CustomSpinnerComponent} from '../../utils/custom-spinner/custom-spinner.component';
 import {Title} from '@angular/platform-browser';
+import {UsuariosResponseDTO} from '../../../dto/UsuariosResponseDTO';
 
 function onlyNumbersAndSpaces(control: AbstractControl): ValidationErrors | null {
   const value = control.value;
@@ -55,11 +56,10 @@ function onlyNumbersAndSpaces(control: AbstractControl): ValidationErrors | null
 })
 export class RegisterComponent implements OnInit{
 
-  mostrarError: Boolean;
-  mostrarSuccess: Boolean;
+  mostrarError: boolean;
   mensaje: String;
+  tipoAlerta: String;
   contraseniasNoIguales: Boolean;
-  errorUniques: Boolean;
   timer: any;
   existeUsuario: Boolean;
   form: FormGroup<{
@@ -71,14 +71,14 @@ export class RegisterComponent implements OnInit{
               private router: Router,
               public functionsUtils: FunctionsUtils, private titleService: Title) {
     this.mostrarError = false;
-    this.mensaje = "";
+    this.mensaje = '';
+    this.tipoAlerta = ''
   }
 
   async ngOnInit(): Promise<void>{
 
     this.service.mostrarSpinner = false;
     this.service.deshabilitarBotones = false;
-    this.mostrarSuccess = false;
     this.form = new FormGroup({
       nombres: new FormControl('', Validators.required),
       apellidos: new FormControl('', Validators.required),
@@ -90,7 +90,6 @@ export class RegisterComponent implements OnInit{
     });
 
     this.contraseniasNoIguales = false;
-    this.errorUniques = false;
     this.titleService.setTitle('Holandesa');
   }
 
@@ -104,14 +103,17 @@ export class RegisterComponent implements OnInit{
       if (contrasenia && confirmarContrasenia) {
         if (contrasenia != confirmarContrasenia){
           this.mensaje = "Las contraseñas no coinciden";
+          this.tipoAlerta = 'danger';
           this.mostrarError = true;
           this.contraseniasNoIguales = true;
         } else {
           this.mensaje = "";
+          this.tipoAlerta = '';
           this.mostrarError = false;
           this.contraseniasNoIguales = false;
         }
       } else {
+        this.tipoAlerta = '';
         this.mostrarError = false;
         this.mensaje = "";
         this.contraseniasNoIguales = false;
@@ -154,29 +156,26 @@ export class RegisterComponent implements OnInit{
 
     const usuario: Usuarios = this.llenarObjeto(this.form);
     const usuariosRequest = new UsuariosRequestDTO('','','','', null, usuario, 0, 0);
-    this.errorUniques = false;
     this.mostrarError = false;
     this.mensaje = '';
-    clearTimeout(this.timer);
-    this.timer = setTimeout(() => {
-      this.errorUniques = false;
-    }, 1000);
-
 
     if (!this.mostrarError){
       this.service.deshabilitarBotones = true;
-      this.mostrarSuccess = true;
       this.service.mostrarSpinner = true;
-      this.service.saveEntity('usuarios', usuario).subscribe(res => {
+      this.service.saveEntity('usuarios', usuario).subscribe((res: UsuariosResponseDTO)=> {
         this.service.mostrarSpinner = false;
         if (res) {
-          this.mensaje = "Usuario registrado con éxito";
+          this.mensaje = res.respuesta;
+          this.mostrarError = true;
+          this.tipoAlerta = res.error ? 'danger' : 'success';
+          console.log('this.tipoAlerta: ', this.tipoAlerta);
           clearTimeout(this.timer);
           this.timer = setTimeout(() => {
-            this.errorUniques = false;
-            this.mostrarSuccess = false;
+            this.mostrarError = false;
             this.service.deshabilitarBotones = false;
-            this.functionsUtils.navigateOption(this.router, 'login');
+            if (!res.error) {
+              this.functionsUtils.navigateOption(this.router, 'login');
+            }
           }, 2400);
         } else {
           this.service.mostrarSpinner = false;
